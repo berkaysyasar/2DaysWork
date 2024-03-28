a6e9d933970a3dbfdbaa1245e81ec9d63019e9bb
Berkay Samet Yasar <berkayyasar0@gmail.com>
2024-03-28 14:55:05 +0300

    all menu items contents done

--- app/src/main/java/com/berkay/a2dayswork/ui/fragment/menuItems/SettingsFragment.kt
+++ app/src/main/java/com/berkay/a2dayswork/ui/fragment/menuItems/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.berkay.a2dayswork.ui.fragment
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
 import androidx.navigation.fragment.findNavController
 import com.berkay.a2dayswork.R
 import com.berkay.a2dayswork.databinding.FragmentRoutineBinding
@@ -14,6 +17,7 @@ import com.berkay.a2dayswork.ui.MainActivity
 
 class SettingsFragment : Fragment() {
     private lateinit var binding: FragmentSettingsBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +25,24 @@ class SettingsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
+
+
+        sharedPreferences = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        // Not switch'in son durumunu al
+        val isNoteSwitchChecked = sharedPreferences.getBoolean("noteswitch", true)
+        val isRoutineSwitchChecked = sharedPreferences.getBoolean("routineswitch", true)
+        val isEnabled = sharedPreferences.getBoolean("enabled", false)
+
+
+
+        binding.noteswitch.setOnCheckedChangeListener { _, isChecked ->
+            sharedPreferences.edit().putBoolean("noteswitch", isChecked).apply()
+        }
+
+        binding.routineswitch.setOnCheckedChangeListener { _, isChecked ->
+            sharedPreferences.edit().putBoolean("routineswitch", isChecked).apply()
+        }
 
         binding.enabledisablebutton.setOnClickListener{
             binding.noteaddTextview.visibility = if (binding.noteaddTextview.visibility == View.VISIBLE) {
@@ -51,8 +73,31 @@ class SettingsFragment : Fragment() {
             }
         }
 
+        val bundle = Bundle()
+        if(isNoteSwitchChecked.equals(true)) {
+            binding.noteswitch.isChecked = true
+            bundle.putInt("noteswitch", 1)
+        }else{
+            binding.noteswitch.isChecked = false
+            bundle.putInt("noteswitch", 0)
+        }
+        if(isRoutineSwitchChecked.equals(true)) {
+            binding.routineswitch.isChecked = true
+            bundle.putInt("routineswitch", 1)
+        }else{
+            binding.routineswitch.isChecked = false
+            bundle.putInt("routineswitch", 0)
+        }
+
+        val routineFragment = RoutineFragment()
+        routineFragment.arguments = bundle
+
+
+        // MainFragment'e geçiş yapmadan önce Bundle'ı ekleyin
+        val mainFragment = MainFragment()
+        mainFragment.arguments = bundle
+
 
         return binding.root
     }
-
 }
