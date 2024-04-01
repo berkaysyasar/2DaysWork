a8c0cc267cb8b4c430c02161f0ef8b0720d2a7d5
Berkay Samet Yasar <berkayyasar0@gmail.com>
2024-04-01 21:46:51 +0300

    empty

--- app/src/main/java/com/berkay/a2dayswork/ui/fragment/ReminderFragment.kt
+++ app/src/main/java/com/berkay/a2dayswork/ui/fragment/ReminderFragment.kt
@@ -29,7 +29,7 @@ class ReminderFragment : Fragment() {
     }
 
     private fun showRemindDialog() {
-        val builder = AlertDialog.Builder(requireContext())
+        val builder = AlertDialog.Builder(requireContext()) 
 
     }
 
